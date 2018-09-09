85f3a790a2c1949620231b94230d9ba2dc3461c3
kopergiesbeek <kopergiesbeek@gmail.com>
2018-09-09 14:58:24 +0200

    test

---
+++ test.py
@@ -0,0 +1,23 @@
+import os
+import wx
+
+import wx.lib.agw.multidirdialog as MDD
+
+# Our normal wxApp-derived class, as usual
+app = wx.App(0)
+
+dlg = MDD.MultiDirDialog(None, title="Custom MultiDirDialog", defaultPath=os.getcwd(),
+                         agwStyle=MDD.DD_MULTIPLE|MDD.DD_DIR_MUST_EXIST)
+
+if dlg.ShowModal() != wx.ID_OK:
+    print("You Cancelled The Dialog!")
+    dlg.Destroy()
+    
+
+paths = dlg.GetPaths()
+for indx, path in enumerate(paths):
+    print("Path %d: %s"%(indx+1, path))
+
+dlg.Destroy()
+
+app.MainLoop()
